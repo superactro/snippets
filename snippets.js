50daca093a262e4180398a569e7e4a9cf2e990f1
Yanko Aleksandrov <superactro@gmail.com>
2015-07-21 17:40:28 +0200

    some snippets added

---
+++ snippets.js
@@ -0,0 +1,56 @@
+function Person(name, age) {
+    // if object created without the 'new' keyword
+    if (!(this instanceof arguments.callee)) {
+        return new arguments.callee(name, age)
+    }
+    this._name = name;
+    this._age = age;
+}
+
+// Console color
+console.log("%cBlue!", "color: blue;");
+
+// do something on click
+$(".element").live("click", function () {
+
+});
+
+var count = 0;
+function myFunction() {
+    count++;
+    if (count > 5) clearInterval(timeout);
+    //do something
+}
+var timeout = setInterval(myFunction, 20000);
+
+if ($("#elementid").length) {
+    //it does!
+}
+
+$(document).ready(function () {
+    $("a.topLink").click(function () {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+        }, {
+            duration: 500,
+            easing: "swing"
+        });
+        return false;
+    });
+});
+
+
+var max_height = 0;
+$("div.col").each(function () {
+    if ($(this).height() > max_height) { max_height = $(this).height(); }
+});
+$("div.col").height(max_height);
+
+
+jQuery.fn.centerElement = function () {
+    this.css("position", "absolute");
+    this.css("top", ($(window).height() - this.height()) / 2 + $(window).scrollTop() + "px");
+    this.css("left", ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + "px")
+    return this;
+}
+$('#item').centerElement();
